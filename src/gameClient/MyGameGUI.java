b768e432bd5914e13bc9bf4540dbfa6a18fd5073
shahareng <shaharbel0@gmail.com>
2020-01-08 13:58:25 +0200

    create MyGameGUI

--- src/gameClient/MyGameGUI.java
+++ src/gameClient/MyGameGUI.java
@@ -1,5 +1,56 @@
 package gameClient;
 
-public class MyGameGUI {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+import com.google.gson.Gson;
+
+public class MyGameGUI {
+	
+	public void drowFruit(String json_file)
+	{
+		Fruit fruit = null;
+		Gson gson = new Gson();
+		try 
+		{
+			FileReader reader = new FileReader(json_file);
+			fruit = gson.fromJson(reader,Fruit.class);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void drowRobot(String json_file)
+	{
+		Robot robot = null;
+		Gson gson = new Gson();
+		try 
+		{
+			FileReader reader = new FileReader(json_file);
+			robot = gson.fromJson(reader,Robot.class);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void drowEdge()
+	{
+		
+	}
+	
+	public void drowNode()
+	{
+		
+	}
+	
+	public void drowTime()
+	{
+		
+	}
 }
